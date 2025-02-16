6c754bec077760d9d1c1c6675db497688e4f454e
Sourabh Singhal <128956043+saasfasterboy@users.noreply.github.com>
2025-02-16 20:45:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import nltk
+from transformers import pipeline 
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+# Download necessary NLTK data
+nltk.download('punkt')
+nltk.download('stopwords')
+
+chatbot = pipeline("text-generation",model="distilgpt2")
+
+
+def healthcare_chatbot(user_input):
+  if "symptom" in user_input:
+    return "Please consult Doctor for accurate advice"
+  elif "appointment" in user_input:
+    return "Would you like to schedule appointment with the Doctor?"
+  elif "medication" in user_input:
+    return "It's important to take prescribed medicines regularly. If you have concerns, consult your doctor."
+  else:
+    response = chatbot(user_input,max_length = 500,num_return_sequences=1)
+    return response[0]['generated_text']
+
+def main():
+  st.title("Healthcare Assistant Chatbot")
+  user_input = st.text_input("How can I assist you today?")
+  if st.button("Submit"):
+    if user_input:
+      st.write("User: ",user_input)
+      response=healthcare_chatbot(user_input)
+      st.write("Healthcare Assistant: ",response)
+
+    else:
+      st.write("Please enter a message to get a response.")
+      
+main()
